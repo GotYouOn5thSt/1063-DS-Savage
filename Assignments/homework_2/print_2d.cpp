b76881ede45bc0034406a03b54eadd0cbb00f2e5
GotYouOn5thSt <ericsavage99@yahoo.com>
2018-01-26 18:27:24 -0800

    Update print_2d.cpp

--- Assignments/homework_2/print_2d.cpp
+++ Assignments/homework_2/print_2d.cpp
@@ -1,1 +1,28 @@
-### new file
+### Homework 2
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	int  i = 0, j = 0, k = 1;
+	int Nums[15][15];
+
+	for (i = 0; i<15; i++) {
+		for (j = 0; j<15; j++) {
+			Nums[i][j] = k;
+			k++;
+		}
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 15; j++) {
+			cout << Nums[j][i] << " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
