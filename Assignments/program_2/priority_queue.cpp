a35ac572e61f1af95d6fd0f30092724412c40df1
GotYouOn5thSt <ericsavage99@yahoo.com>
2018-04-02 07:38:05 -0700

    Update priority_queue.cpp

--- Assignments/program_2/priority_queue.cpp
+++ Assignments/program_2/priority_queue.cpp
@@ -1,37 +1,25 @@
 ///////////////////////////////////////////////////////////////////////////////
-//                   YOU MUST COMPLETE AND COMMENT YOUR CODE!
-// Title:            (program's title)
-// Files:            (list of source files)
-// Semester:         (course) Spring 2018
+// Title:            Program 2
+// Files:            priority_queue.cpp
+//                   priority_out.txt
+//                   input_data.txt
+// Semester:         1063 Spring 2018
 //
-// Author:           (your name)
-// Email:            (your email address)
+// Author:           Roderic Savage
+// Email:            ericsavage99@gmail.com
 // Description:
-//       describe program here thoroughly
+//       This programs takes an input file of 450 rows and 2 columns. This first
+//       column is a command of "push" or "pop". The second column is an animal
+//       name. A queue is created, and the information is then pushed onto and
+//       popped off of the queue per the command from the first column of the
+//       file. The items in the queue are also re-arranged by length and 
+//       alphabetical position. What remains in the queue is printed via a 
+//       method titled "print", and items popped from the queue are printed to 
+//       the output file titled "priority_out.txt"
 /////////////////////////////////////////////////////////////////////////////////
 
 
-/**
-* Function Push:
-*      Pushes an item onto the stack.
-*
-* Params:
-*      [int] val - value of item being pushed
-* Returns
-*      void
-*/
 
-/////////////////////////////////
-// List Based Queue
-//
-// First In First Out
-//
-// Method:
-//    void push()
-//    string pop()
-//    bool empty()
-//    bool full() ?????
-/////////////////////////////////
 
 #include <iostream>
 #include <fstream>
@@ -39,6 +27,15 @@
 
 using namespace std;
 
+/**
+* Function countLen:
+*      counts characters in string.
+*
+* Params:
+*      [string] val - value of item being pushed
+* Returns
+*      int
+*/
 int countLen(char *word)
 {
 	int l = 0;
@@ -66,18 +63,24 @@ struct Node
 	}
 };
 
-/**
-* class Queue
-*     description of what this class does ...
-* Methods:
-*  - Queue()       : constructor
-*  - string pop()  :
-*/
+
+// List Based Queue
+//
+// First In First Out
+//
+// Method:
+//    void push()
+//    string pop()
+//    bool pop()
+//    bool empty()
+//    void print() 
+//	  void sort()
+
 class Queue
 {
 private:
 	Node *Front;
-Node *Rear;
+	Node *Rear;
 
 public:
 	/**
@@ -93,8 +96,13 @@ public:
 	}
 
 	/**
-	* pop
+	* Function pop:
+	*      pops item off of queue.
 	*
+	* Params:
+	*      none
+	* Returns
+	*      string
 	*/
 	string pop()
 	{
@@ -112,7 +120,15 @@ public:
 			return " ";
 		}
 	}
-
+	/**
+	* Function push:
+	*      pushes item onto queue.
+	*
+	* Params:
+	*      [string] v - value inside of node
+	* Returns:
+	*      none
+	*/
 	void push(string v)
 	{
 		Node *n = new Node(v);
@@ -131,22 +147,50 @@ public:
 		}
 	}
 
+	/**
+	* Function empty:
+	*      tests for empty queue
+	*
+	* Params:
+	*      none
+	* Returns
+	*      bool
+	*/
 	bool empty()
 	{
 		return (Front == NULL);
 	}
 
+	/**
+	* Function print:
+	*      prints popped items onto output file.
+	*
+	* Params:
+	*      none
+	* Returns
+	*      none
+	*/
 	void print()
 	{
 		Node *temp = Front;
+		int count = 1;
 		while (temp)
 		{
-			cout << temp->val << "->";
+			cout << count++ << " " << temp->val << endl;
 			temp = temp->next;
 		}
 		cout << endl;
 	}
 
+	/**
+	* Function pop:
+	*      pops items off of queue
+	*
+	* Params:
+	*      [string] &v - value of node
+	* Returns:
+	*      bool
+	*/
 	bool pop(string &v)
 	{
 		if (!empty())
@@ -165,29 +209,37 @@ public:
 		}
 	}
 
+	/**
+	* Function sort:
+	*      sorts queue by length and alphabet
+	*
+	* Params:
+	*      none
+	* Returns:
+	*      none
+	*/
 	void sort()
 	{
-		string animal1, animal2, tempAnimal;
-		Node *temp = Front;
-		while (temp->next != NULL)
+		string tempAnimal = "";    // created temporary variable for swap
+		Node* animal1 = Front;     // pointer initialized to "Front"
+		Node* animal2 = Front;     // pointer initialized to "Front"
+		bool swapped = false;      // setting condition to test for later
+		while (animal1)
 		{
-			animal1 = temp->val;
-			animal2 = temp->next->val;
-
-			if (animal1.length() > animal2.length()) {
-				tempAnimal = animal1;
-				animal1 = animal2;
-				animal2 = tempAnimal;
-			}
-
-			else if (animal1.length() == animal2.length()) {
-				if (animal1 > animal2) {
-					tempAnimal = animal1;
-					animal1 = animal2;
-					animal2 = tempAnimal;
+			animal2 = animal1->next;   // guarantees that animal2 is ahead of ...
+			while (animal2) {          // animal1 while traversing list
+				if (animal1->val.length() > animal2->val.length()) {
+					swapped = true;
+					tempAnimal = animal1->val;     // the next three lines
+					animal1->val = animal2->val;   // are the swap
+					animal2->val = tempAnimal;
 				}
+				animal2 = animal2->next;   // iterates to the next node
 			}
-			temp = temp->next;
+			animal1 = animal1->next;    // iterates to the next node
+		}
+		if (!swapped) {
+			return;
 		}
 	}
 };
@@ -201,29 +253,29 @@ int main()
 	ifstream infile;
 	ofstream outfile;
 
-	infile.open("input_data.txt");
-	outfile.open("priority_out.txt");
+	infile.open("input_data.txt");     // input information from external source
+	outfile.open("priority_out.txt");  // output information to external source
 
 	outfile << "Animals Popped off the Queue: \n \n";
-	
-	while (!infile.eof()) {
+
+	while (!infile.eof()) {    // this loop separates the list by initial command
 		infile >> pushOrPop;
 		if (pushOrPop == "push") {
 			infile >> animal;
-			Q.push(animal);
+			Q.push(animal);    // pushes onto queue if command equals "push"
 		}
 		else {
 			outfile << count++ << " " << animal << endl;
-			Q.pop(animal);
+			Q.pop(animal);    // pops from queue if command equals "pop"
 		}
 	}
-	
-	
-	cout << "Animals that remained on the Queue: \n \n";
-	Q.sort();
-	Q.print();
 
-	
+
+	cout << "Animals remaining on the Queue (in order of priority): \n \n";
+	Q.sort();                 // sorts list in queue by length and alphabet
+	Q.print();                // prints "popped" to output file 
+
+
 
 	infile.close();
 	outfile.close();
